612af58708ccf315746a036b84354027fa9d26e0
bradfaas <brad.faas@gmail.com>
2025-08-14 03:13:47 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,11 @@ variable "region"                { type = string }
 variable "lab_id"                { type = string }
 variable "user_id"               { type = string }
 variable "s3_app_bucket"         { type = string }
-variable "domain_admin_password" { type = string  sensitive = true }
+variable "domain_admin_password" {
+  type      = string
+  sensitive = true
+}
+
 
 variable "windows_apps" {
   type = list(object({ s3_key = string, args = string }))
