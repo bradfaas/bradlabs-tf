56faa03394b1e61bdb9e0a026bfac95a0457ba98
bradfaas <brad.faas@gmail.com>
2025-08-14 16:26:46 -0500

    Update outputs.tf

--- modules/lab/outputs.tf
+++ modules/lab/outputs.tf
@@ -10,3 +10,13 @@ output "instance_ids" {
     linux = aws_instance.linux.id
   }
 }
+output "nlb_public_ip" {
+  description = "Public IPv4 of the NLB (use :3389 for Windows, :3390 for Linux)"
+  value       = try(aws_eip.nlb[0].public_ip, null)
+}
+
+output "nlb_dns_name" {
+  description = "Public DNS name of the NLB"
+  value       = try(aws_lb.rdp[0].dns_name, null)
+}
+
